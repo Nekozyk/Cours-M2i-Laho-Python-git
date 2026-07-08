67b68ced02bf4504ce61bc9789c51ab632f764b1
nicolas.grz <nicolas.grzegorczyk@labom2iformation.fr>
2026-07-08 12:04:11 +0200

    Exemple variable

---
+++ HelloWorld/Variable.py
@@ -0,0 +1,10 @@
+# petit programme qui demande à l'utilisateur de saisir un texte, puis affiche ce texte ainsi que d'autres variables.
+mon_texte = input("Entrez un texte : ")
+mon_nombre = 42
+mon_nombre_decimal = 3.14
+mon_booleen = True  
+
+print(mon_texte)
+print(mon_nombre)
+print(mon_nombre_decimal)
+print(mon_booleen)
