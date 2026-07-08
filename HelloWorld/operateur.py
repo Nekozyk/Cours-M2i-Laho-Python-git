8d818b4b554c5b574717e6efaee48d1b691c7750
nicolas.grz <nicolas.grzegorczyk@labom2iformation.fr>
2026-07-08 13:29:38 +0200

    operateur.py

---
+++ HelloWorld/operateur.py
@@ -0,0 +1,20 @@
+nombre_un = float(input("Entrez un nombre : "))
+nombre_deux = float(input("Entrez un autre nombre : "))
+
+la_somme = nombre_un + nombre_deux
+
+print("La somme de", nombre_un, "et", nombre_deux, "est égale à", la_somme)
+
+la_difference = nombre_un - nombre_deux
+print("La différence de", nombre_un, "et", nombre_deux, "est égale à", la_difference)
+
+le_quotient = nombre_un / nombre_deux
+print("Le quotient de", nombre_un, "et", nombre_deux, "est égale à", le_quotient)
+le_quotient_entier = nombre_un // nombre_deux
+print("Le quotient entier de", nombre_un, "et", nombre_deux, "est égale à", le_quotient_entier)
+
+le_modulo = nombre_un % nombre_deux
+print("Le modulo de", nombre_un, "et", nombre_deux, "est égale à", le_modulo)
+
+le_produit = nombre_un * nombre_deux
+print("Le produit de", nombre_un, "et", nombre_deux, "est égale à", le_produit)
